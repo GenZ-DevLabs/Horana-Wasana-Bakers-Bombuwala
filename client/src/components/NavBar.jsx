b58f6c3d38d3f0473758e486adc55cb362ee0bcd
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-07-30 11:18:26 +0530

    add toggle to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
 import Logo from "../assets/logo.png";
 
 export default function Header() {
   const { user } = useContext(UserContext);
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div className="fixed w-full bg-primary text-white px-8 py-2 z-20">
       <header className="flex justify-between">
@@ -17,7 +19,7 @@ export default function Header() {
             HWB Bombuwala
           </span>
         </Link>
-        <div className="flex items-center md:text-md lg:text-lg">
+        <div className="hidden md:flex items-center md:text-md lg:text-lg ">
           <Link className="mr-4 " to={"/"}>
             Home
           </Link>
@@ -38,6 +40,62 @@ export default function Header() {
             </span>
             <div className="bg-gray-500 text-white rounded-full overflow-hidden"></div>
           </Link>
+        </div>
+        <div className="flex md:hidden flex-1 justify-end items-center">
+          {showMenu ? (
+            <svg
+              xmlns={"http://www.w3.org/2000/svg"}
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+              onClick={() => setShowMenu(!showMenu)}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          )}
+
+          <div
+            className={`${
+              !showMenu ? "hidden" : "flex"
+            } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-2xl bg-primary`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              <li onClick={() => setShowMenu(!showMenu)}>
+                <Link to="/">Home</Link>
+              </li>
+              <li onClick={() => setShowMenu(!showMenu)}>
+                <Link to="/about">About</Link>
+              </li>
+              <li onClick={() => setShowMenu(!showMenu)}>
+                <Link to="/contactus">Contact Us</Link>
+              </li>
+              <li onClick={() => setShowMenu(!showMenu)}>
+                <Link to={user ? "/account" : "/login"}>Admin</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </header>
     </div>
