5d1f47014e97de601cea981374bb25aa96bcff80
HoundSL <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-08-10 11:04:20 +0530

    edit mobile type 2

--- client/src/components/DesignBoard.jsx
+++ client/src/components/DesignBoard.jsx
@@ -21,7 +21,7 @@ export default function DesignBoard() {
   return (
     <div className="pt-24 flex justify-center">
       <BackButton />
-      <div className="pt-12 grid gap-4 justify-center items-center sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <div className="pt-12 grid gap-4 justify-center items-center grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {designs.length > 0 &&
           designs.map((design) => (
             <>
@@ -31,7 +31,8 @@ export default function DesignBoard() {
                 ) && (
                   <Link
                     key={design._id}
-                    className="flex flex-col mx-5 my-5 rounded-2xl mt-3 w-64 shadow-lg shadow-gray-500"
+                    // className="flex flex-col mx-5 my-5 rounded-2xl mt-3 w-64 shadow-lg shadow-gray-500"
+                    className="flex cursor-pointer mx-3 sm:mx-5 my-5 rounded-2xl mt-3 w-[170px] sm:w-64 shadow-lg shadow-gray-500"
                     to={
                       "/" +
                       design.board.replace(/\s+/g, "").toLowerCase() +
@@ -40,12 +41,21 @@ export default function DesignBoard() {
                     }
                   >
                     <img
-                      className="flex w-64 h-64 items-center justify-around  object-cover overflow-hidden rounded-t-2xl"
+                      //className="flex w-64 h-64 items-center justify-around  object-cover overflow-hidden rounded-t-2xl"
+                      className="flex w-43 h-43 sm:w-64 sm:h-64 items-center justify-around  object-cover overflow-hidden rounded-t-2xl"
                       src={defaultURL + "uploads/" + design.photos[0]}
                       alt="design"
                     />
-                    <div className="flex items-center justify-around w-64 h-16 bg-secondary text-black rounded-b-2xl font-semibold">
-                      <h1 className="text-lg font-semibold">{design.title}</h1>
+                    <div
+                      //className="flex items-center justify-around w-64 h-16 bg-secondary text-black rounded-b-2xl font-semibold"
+                      className="flex items-center justify-around  sm:w-64 h-16 bg-secondary text-black rounded-b-2xl font-semibold"
+                    >
+                      <h1
+                        //className="text-lg font-semibold"
+                        className="text-sm sm:text-md text-center"
+                      >
+                        {design.title}
+                      </h1>
                     </div>
                   </Link>
                 )}
